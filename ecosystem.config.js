acad1d3a87b91ddac65e64d9447f4630e7c36631
rummik <k@9k1.us>
2017-12-05 03:00:54 -0500

    Things

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: 'WEB',
+      script: 'app.js',
+      env_production : {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'pi',
+      host: '10.17.2.69',
+      ref: 'origin/master',
+      repo: 'https://github.com/rummik/webremote.git',
+      path: '/home/pi/webremote',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
