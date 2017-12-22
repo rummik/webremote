f9d28b94b69c90e5468f07399d74b117c36f5de2
rummik <k@9k1.us>
2017-12-22 18:32:15 -0500

    Fix host bits

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const serve = require('koa-static');
 const router = new Router({ prefix: '/lirc' });
 const app = new Koa();
 
-const lirc = new Lirc({ host: '10.17.2.69' });
+const lirc = new Lirc();
 
 lirc.on('connect', () =>
   console.log('connected to lirc')
